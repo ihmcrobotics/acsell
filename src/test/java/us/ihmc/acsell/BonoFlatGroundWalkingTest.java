b1b6aa443ebc8cee284755733b0ffb92992e4014
Duncan Calvert <dcalvert@ihmc.us>
2019-02-06 13:16:06 -0600

    Remove unused imports.

--- src/test/java/us/ihmc/acsell/BonoFlatGroundWalkingTest.java
+++ src/test/java/us/ihmc/acsell/BonoFlatGroundWalkingTest.java
@@ -4,11 +4,8 @@ import org.junit.jupiter.api.Test;
 
 import us.ihmc.avatar.DRCFlatGroundWalkingTest;
 import us.ihmc.avatar.drcRobot.DRCRobotModel;
-import us.ihmc.continuousIntegration.ContinuousIntegrationAnnotations.ContinuousIntegrationPlan;
-import us.ihmc.continuousIntegration.ContinuousIntegrationAnnotations.ContinuousIntegrationTest;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Disabled;
-import us.ihmc.continuousIntegration.IntegrationCategory;
 import us.ihmc.robotics.controllers.ControllerFailureException;
 import us.ihmc.simulationConstructionSetTools.bambooTools.BambooTools;
 import us.ihmc.simulationconstructionset.util.simulationRunner.BlockingSimulationRunner.SimulationExceededMaximumTimeException;
